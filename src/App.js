04f0872e5453fb70aba04fb51885cd669f9eaf9d
Wonwoo Park <gn030108@gmail.com>
2024-06-02 18:58:38 +0900

    스크롤 이동 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import './App.css';
 import Navbar from './component/Navbar';
 import About from './component/About';
@@ -7,14 +8,25 @@ import Top from './component/Top';
 import Contact from './component/Contact';
 
 function App() {
+  const navbarRef = useRef(null); // 네비게이션 바의 ref
+  const aboutRef = useRef(null);
+  const skillRef = useRef(null);
+  const projectRef = useRef(null);
+  const contactRef = useRef(null);
+
+
+
   return (
     <div className="App">
-      <header><Navbar/></header>
-      <Top/>
-      <About/>
-      <Skill/>
-      <Project/>
-      <Contact/>
+      <header ref={navbarRef}>
+        <Navbar 
+          refs={{navbarRef, aboutRef, skillRef, projectRef, contactRef}}/>
+      </header>
+      <Top />
+      <section ref={aboutRef}><About/></section>
+      <section ref={skillRef}><Skill/></section>
+      <section ref={projectRef}><Project/></section>
+      <section ref={contactRef}><Contact/></section>
     </div>
   );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,13 +1,20 @@
-
-
-
 import { useState } from 'react'
 import styles from '../styles/Navbar.module.scss'
 
-const Navbar = () => {
+const Navbar = ({refs}) => {
 
   const [handleMenu, setHandleMenu] = useState(false)
 
+  const scrollToSection = (sectionRef) => {
+    const navbarHeight = refs.navbarRef.current.offsetHeight; // 네비게이션 바의 높이
+    const sectionTop = sectionRef.current.offsetTop; // 섹션의 상단 위치
+    window.scrollTo({ top: sectionTop - navbarHeight, behavior: 'smooth' });
+    setHandleMenu(false)
+  };
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    setHandleMenu(false)
+  };
 
   return (
     <div className={styles.component}>
@@ -15,10 +22,11 @@ const Navbar = () => {
         <div>Pww's Portfolio</div>
       </div>
       <div className={styles.wide}>
-        <span>About</span>
-        <span>Skills</span>
-        <span>Project</span>
-        <span>Contact</span>
+        <span onClick={()=>(scrollToTop())}>Top</span>
+        <span onClick={()=>(scrollToSection(refs.aboutRef))}>About</span>
+        <span onClick={()=>(scrollToSection(refs.skillRef))}>Skills</span>
+        <span onClick={()=>(scrollToSection(refs.projectRef))}>Project</span>
+        <span onClick={()=>(scrollToSection(refs.contactRef))}>Contact</span>
       </div>
       <div className={styles.thin}>
         <i className="ri-menu-line" onClick={()=>(setHandleMenu(!handleMenu))}></i>
@@ -29,10 +37,11 @@ const Navbar = () => {
           <i className="ri-close-fill"></i>
         </div>
         <div className={styles.side_menu}>
-          <div><p>About</p></div>
-          <div><p>Skills</p></div>
-          <div><p>Project</p></div>
-          <div><p>Contact</p></div>
+          <div><p onClick={()=>(scrollToTop())}>Top</p></div>
+          <div><p onClick={()=>(scrollToSection(refs.aboutRef))}>About</p></div>
+          <div><p onClick={()=>(scrollToSection(refs.skillRef))}>Skills</p></div>
+          <div><p onClick={()=>(scrollToSection(refs.projectRef))}>Project</p></div>
+          <div><p onClick={()=>(scrollToSection(refs.contactRef))}>Contact</p></div>
         </div>
       </div>
     </div>
