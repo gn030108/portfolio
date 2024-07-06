d29e4f711cf8b2772f51b1cf8584bd5ba03fa2f0
Wonwoo Park <gn030108@gmail.com>
2024-07-06 16:26:13 +0900

    프로필 사진 변경

--- src/component/About.js
+++ src/component/About.js
@@ -1,9 +1,6 @@
-import 카리나 from '../image/카리나.png'
-import {ReactComponent as 졸업} from '../image/svg/졸업1.svg'
-
-
-import styles from '../styles/About.module.scss'
-
+import { ReactComponent as 졸업 } from "../image/svg/졸업1.svg";
+import 사진 from "../image/사진.jpg";
+import styles from "../styles/About.module.scss";
 
 const About = () => {
   return (
@@ -12,7 +9,7 @@ const About = () => {
         <div className={styles.header}>ABOUT ME</div>
         <div className={styles.profile}>
           <div className={styles.profile_img}>
-            <img src={카리나}/>
+            <img src={사진} />
           </div>
           <div className={styles.profile_text}>
             <li>
@@ -32,19 +29,18 @@ const About = () => {
               <p>gn030108@gmail.com</p>
             </li>
             <li>
-              <졸업 className={styles.school}/>
+              <졸업 className={styles.school} />
               <p>동양미래대학교 (정보전자공학과)</p>
             </li>
             <li className={styles.profile_last}>
               <i className="ri-github-fill"></i>
-              <a href='https://github.com/gn030108'>GitHub</a>
+              <a href="https://github.com/gn030108">GitHub</a>
             </li>
-            
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
